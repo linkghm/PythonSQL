e5559c9fd839bbcda2f5fcc9408ab227a14cdb7d
linkghm <link2619@126.com>
2019-06-07 00:38:20 +0800

    create & connect to SQL

---
+++ Create&Connect.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/7 0:37
+# @Author  : GUO Huimin
+# @Email   : guohuimin2619@foxmail.com
+# @FileName: Create&Connect.py
+
+# Python MySQL Tutorials from https://pynative.com/python-mysql-tutorial/
+
