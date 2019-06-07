662a839bcb51e2789a0c60b0459b8a6cbe083919
linkghm <link2619@126.com>
2019-06-07 11:32:46 +0800

    user_pure

--- Create&Connect.py
+++ Create&Connect.py
@@ -11,52 +11,52 @@ from mysql.connector import Error
 from mysql.connector import errorcode
 
 # 1 simple example
-# try:
-#     connection = mysql.connector.connect(host='localhost',
-#                                          database='python_db',
-#                                          user='root',
-#                                          password='root1234')
-#     if connection.is_connected():
-#         db_INFO = connection.get_server_info()
-#         print('Connected to MySQL database... MySQL Server version on', db_INFO)
-#         cursor = connection.cursor()
-#         cursor.execute('select database();')
-#         record = cursor.fetchone()
-#         print('Your connected to - ', record)
-# except Error as e:
-#     print('Error', e)
-# finally:
-#     if (connection.is_connected()):
-#         cursor.close()
-#         connection.close()
-#         print('MySQL is closed')
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='python_db',
+                                         user='root',
+                                         password='root1234')
+    if connection.is_connected():
+        db_INFO = connection.get_server_info()
+        print('Connected to MySQL database... MySQL Server version on', db_INFO)
+        cursor = connection.cursor()
+        cursor.execute('select database();')
+        record = cursor.fetchone()
+        print('Your connected to - ', record)
+except Error as e:
+    print('Error', e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print('MySQL is closed')
 
 # 2 Use the Dictionary to keep MySQL Connection arguments in Python
-# connection_config_dict = {
-#     'user': 'root',
-#     'password': 'root1234',
-#     'host': 'localhost',
-#     'database': 'python_db',
-#     'raise_on_warnings': True,
-#     'use_pure': False,
-#     'autocommit': True,
-#     'pool_size': 5
-# }
-# try:
-#     connection = mysql.connector.connect(**connection_config_dict)
-#     if connection.is_connected():
-#         db_INFO = connection.get_server_info()
-#         print('Connected to MySQL database... MySQL Server version on', db_INFO)
-#         cursor = connection.cursor()
-#         cursor.execute('select database();')
-#         record = cursor.fetchone()
-#         print('Your connected to - ', record)
-# except Error as e:
-#     print('Error', e)
-# finally:
-#     if (connection.is_connected()):
-#         cursor.close()
-#         connection.close()
+connection_config_dict = {
+    'user': 'root',
+    'password': 'root1234',
+    'host': 'localhost',
+    'database': 'python_db',
+    'raise_on_warnings': True,
+    'use_pure': False,
+    'autocommit': True,
+    'pool_size': 5
+}
+try:
+    connection = mysql.connector.connect(**connection_config_dict)
+    if connection.is_connected():
+        db_INFO = connection.get_server_info()
+        print('Connected to MySQL database... MySQL Server version on', db_INFO)
+        cursor = connection.cursor()
+        cursor.execute('select database();')
+        record = cursor.fetchone()
+        print('Your connected to - ', record)
+except Error as e:
+    print('Error', e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
 
 # 3 Change the MySQL connection timeout when connecting through Python
 try:
