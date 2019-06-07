40ddb2debfcf17712b5e893ab26696f29655aac7
linkghm <link2619@126.com>
2019-06-07 14:00:07 +0800

    Create table and insert data

--- InsertData.py
+++ InsertData.py
@@ -6,3 +6,27 @@
 
 # Python MySQL Tutorials from https://pynative.com/python-mysql-tutorial/
 
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        user='root',
+        password='root1234',
+        host='localhost',
+        database='python_db'
+    )
+    # warning: differences between ` and '
+    sql_insert_query = """ INSERT INTO `python_user` (`id`,`name`,`birth_date`,`age`) VALUES (1,'Scott','2019-06-07',26)"""
+    cursor = connection.cursor()
+    result = cursor.execute(sql_insert_query)
+    connection.commit()
+    print('Record inserted successfully into python_user table')
+except Error as e:
+    connection.rollback()
+    print('Error', e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print('MySQL is closed')
