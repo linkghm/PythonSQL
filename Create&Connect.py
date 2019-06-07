110e3132e6f8b311ebd54323f562f33ca7f779d3
linkghm <link2619@126.com>
2019-06-07 11:25:36 +0800

    connection timeout

--- Create&Connect.py
+++ Create&Connect.py
@@ -10,7 +10,7 @@ import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
 
-# simple example
+# 1 simple example
 # try:
 #     connection = mysql.connector.connect(host='localhost',
 #                                          database='python_db',
@@ -31,30 +31,57 @@ from mysql.connector import errorcode
 #         connection.close()
 #         print('MySQL is closed')
 
-# Use the Dictionary to keep MySQL Connection arguments in Python
-connection_config_dict = {
-    'user': 'root',
-    'password': 'root1234',
-    'host': 'localhost',
-    'database': 'python_db',
-    'raise_on_warnings': True,
-    'use_pure': False,
-    'autocommit': True,
-    'pool_size': 5
-}
+# 2 Use the Dictionary to keep MySQL Connection arguments in Python
+# connection_config_dict = {
+#     'user': 'root',
+#     'password': 'root1234',
+#     'host': 'localhost',
+#     'database': 'python_db',
+#     'raise_on_warnings': True,
+#     'use_pure': False,
+#     'autocommit': True,
+#     'pool_size': 5
+# }
+# try:
+#     connection = mysql.connector.connect(**connection_config_dict)
+#     if connection.is_connected():
+#         db_INFO = connection.get_server_info()
+#         print('Connected to MySQL database... MySQL Server version on', db_INFO)
+#         cursor = connection.cursor()
+#         cursor.execute('select database();')
+#         record = cursor.fetchone()
+#         print('Your connected to - ', record)
+# except Error as e:
+#     print('Error', e)
+# finally:
+#     if (connection.is_connected()):
+#         cursor.close()
+#         connection.close()
+
+# 3 Change the MySQL connection timeout when connecting through Python
 try:
-    connection = mysql.connector.connect(**connection_config_dict)
+    connection = mysql.connector.connect(user='root', password='root1234', \
+                                         host='localhost',
+                                         database='python_db',
+                                         connection_timeout=180)
     if connection.is_connected():
         db_INFO = connection.get_server_info()
-        print('Connected to MySQL database... MySQL Server version on', db_INFO)
+        print('Connected to the server, the version is ', db_INFO)
         cursor = connection.cursor()
-        cursor.execute('select database();')
-        record = cursor.fetchone()
-        print('Your connected to - ', record)
+        global_connection_timeout = 'SET GLOBAL connect_timeout=180'
+        global_wait_timeout = 'SET GLOBAL connect_timeout=180'
+        global_interactive_timeout = 'SET GLOBAL connect_timeout=180'
+
+        cursor.execute(global_connection_timeout)
+        cursor.execute(global_wait_timeout)
+        cursor.execute(global_interactive_timeout)
+
+        connection.commit()
+
+    pass
 except Error as e:
     print('Error', e)
 finally:
     if (connection.is_connected()):
         cursor.close()
         connection.close()
-# 
