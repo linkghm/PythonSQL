43b35807800803b22195bb9074eeb5944ac529f5
linkghm <link2619@126.com>
2019-06-07 11:35:40 +0800

    initial InserData.py

---
+++ InsertData.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/7 11:34
+# @Author  : GUO Huimin
+# @Email   : guohuimin2619@foxmail.com
+# @FileName: InsertData.py
+
+# Python MySQL Tutorials from https://pynative.com/python-mysql-tutorial/
+
