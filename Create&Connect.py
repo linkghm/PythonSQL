8274a204ca89a7bfda1bd036076e7f5eb4aaad60
linkghm <link2619@126.com>
2019-06-07 09:40:24 +0800

    create & connect to SQL

--- Create&Connect.py
+++ Create&Connect.py
@@ -6,3 +6,25 @@
 
 # Python MySQL Tutorials from https://pynative.com/python-mysql-tutorial/
 
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='python_db',
+                                         user='root',
+                                         password='root1234')
+    if connection.is_connected():
+        db_INFO = connection.get_server_info()
+        print('Connected to MySQL database... MySQL Server version on ",db_Info')
+        cursor = connection.cursor()
+        cursor.execute('select database();')
+        record = cursor.fetchone()
+        print('Your connected to - ',record)
+except Error as e:
+    print('Error',e)
+finally:
+    if(connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print('MySQL is closed')
